5ef4133fbd297e2a6db657f2209f0d13b7b0447b
Gerardo Cicalese <92589816+mazamin7@users.noreply.github.com>
2023-11-30 16:39:12 +0100

    Minor improvement

--- source/Source.cpp
+++ source/Source.cpp
@@ -34,11 +34,14 @@ double Simulation::duration_ = 2;		// Duration of the whole simulation (seconds)
 //double Simulation::dh_ = 0.1;
 //double Simulation::dt_ = 1.25e-4;
 
-double Simulation::dh_ = 0.2;
-double Simulation::dt_ = 2e-4;
+//double Simulation::dh_ = 0.2;
+//double Simulation::dt_ = 2e-4;
 
 //double Simulation::dh_ = 0.5;
 //double Simulation::dt_ = 6.25e-4;
+
+double Simulation::dh_ = 0.1;
+double Simulation::dt_ = 0.0025;
 
 double Simulation::c0_ = 10;// 3.435e2;		// Speed of sound
 int Simulation::n_pml_layers_ = 10;		// Number of pml layers.
